bdfb4c5e4c74aa42e3eb03672f1bfc6f95928b68
Kittipong <kittipong.nuanyai@gmail.com>
2016-04-26 14:56:56 +0700

    finish desing singnup screen

--- app/src/main/java/ice_pbru/kittipongnuanyai/ice_database/MainActivity.java
+++ app/src/main/java/ice_pbru/kittipongnuanyai/ice_database/MainActivity.java
@@ -1,13 +1,41 @@
 package ice_pbru.kittipongnuanyai.ice_database;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+     //bind widget
+        bindwidget();
+
+
+
+    } //Main Method
+
+    public void clickRegis(View view) {
+        startActivity(new Intent(MainActivity.this, SignUp.class));
+
     }
-}
+
+    public void testMyAlert(View view) {
+        MyAlert myAlert = new MyAlert();
+        myAlert.myDialog(this,"Alert","ทดสอบ");
+
+    }
+
+    private void bindwidget() {
+
+
+    }//bind widget
+
+
+}//Main Class
